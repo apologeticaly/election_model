ca3394bd49f5f0edc64c8baf96c72a6242526999
Constantino <muheec@gmail.com>
2020-02-11 13:49:58 -0800

    sys.argv[k, s]

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import random
 from states import states
 from terminaltables import AsciiTable, SingleTable
+import sys
 
 results = []
 
@@ -34,17 +35,7 @@ def state_election():
     
     if sum(democrat_electors) < sum(republican_electors):
         results.append('R')
-
-    # print(sum(democrat_electors), sum(republican_electors))
-    # print(sum(democrat_electors) + sum(republican_electors))
-            
-
-for _ in range(10000):
-    democrat_electors = []
-    republican_electors = []
-    state_election()
     
-
 def win_probability ():
     democrat_probability = results.count('D') / 100
     republican_probability = results.count('R') / 100
@@ -55,10 +46,31 @@ def win_probability ():
     [str(democrat_probability) + '%', str(republican_probability) + '%', str(tie_probability) + '%']
     ]
     table = SingleTable(table_data)
-    table.title = 'General Election Chances'
+    table.title = 'GE Chances [k=10000]'
     table.justify_columns[0] = 'center' 
     table.justify_columns[1] = 'center'
     table.justify_columns[2] = 'center'  
+
     print (table.table)
 
-win_probability()
+if __name__ == '__main__':
+    if sys.argv[1] == 'k':
+        for _ in range(10000):
+            democrat_electors = []
+            republican_electors = []
+            state_election()
+        win_probability()
+
+    if sys.argv[1] == 's':
+        state_election()
+        se_table_data = [
+        ['DEMOCRAT', 'REPUBLICAN', 'TOTAL'],
+        [str(sum(democrat_electors)), str(sum(republican_electors)), str(sum(democrat_electors) + sum(republican_electors))]
+        ]
+        se_table = SingleTable(se_table_data)
+        se_table.title = 'GE Simulation'
+        se_table.justify_columns[0] = 'center' 
+        se_table.justify_columns[1] = 'center'
+        se_table.justify_columns[2] = 'center'  
+
+        print (se_table.table)
