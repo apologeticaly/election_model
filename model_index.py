d36c407069627c7a47a8bb9c0fced42adbbf9bc6
Constantino <muheec@gmail.com>
2020-02-13 11:55:43 -0800

    lots

---
+++ model_index.py
@@ -0,0 +1,103 @@
+import random
+import numpy
+from contests import contests
+from terminaltables import AsciiTable, SingleTable
+import sys
+
+scores = {}
+
+
+def model_index():
+    for key, value in contests.items():
+        democrat = 'D'
+        republican = 'R'
+        mi_score_d = 0
+        mi_score_r = 0
+        polling_d = int(value[1])
+        polling_r = int(value[2])
+        mle_d = float(value[4])
+        mle_r = float(value[5])
+        favor_d = float(value[6])
+        favor_r = float(value[7])
+        past_five = value[8]
+
+        if numpy.subtract(polling_d, polling_r) > 10:
+            mi_score_d = mi_score_d + 25
+
+        if numpy.subtract(polling_d, polling_r) > 5:
+            mi_score_d = mi_score_d + 10
+        
+        if numpy.subtract(polling_d, polling_r) > 1:
+            mi_score_d = mi_score_d + 5
+        
+        if numpy.subtract(polling_r, polling_d) > 10:
+            mi_score_r = mi_score_r+ 25
+
+        if numpy.subtract(polling_r, polling_d) > 5:
+            mi_score_r = mi_score_r + 10
+        
+        if numpy.subtract(polling_r, polling_d) > 1:
+            mi_score_r = mi_score_r + 5
+
+
+
+        for i in past_five: 
+            if i == 'D': 
+                mi_score_d = mi_score_d + 5
+
+        for i in past_five: 
+            if i == 'R': 
+                mi_score_r = mi_score_r + 5
+
+
+
+        if mle_d > mle_r:
+            mi_score_d = mi_score_d + 5
+
+            if mle_d > 5:
+                mi_score_d = mi_score_d + 5
+
+            if mle_d > 10:
+                mi_score_d = mi_score_d + 5
+
+        if mle_r > mle_d:
+            mi_score_r = mi_score_r + 5
+
+            if mle_d > 5:
+                mi_score_r = mi_score_r + 5
+
+            if mle_d > 10:
+                mi_score_r = mi_score_r + 5
+        
+
+
+        # if favor_d > favor_r:
+        #     mi_score_d = mi_score_d + 10
+
+        # if favor_r > favor_d :
+        #     mi_score_r = mi_score_r + 10
+    
+        scores[key] = [mi_score_d, mi_score_r]
+        mi_score_d = 0
+        mi_score_r = 0
+
+model_index()
+
+table_data = []
+
+# def return_tabulated():
+#     for key, value in scores.items():
+#         table_data.append([key, value[0], value[1]])
+    
+#     table_data.insert(0, ['STATE', 'DEMOCRAT', 'REPUBLICAN'])
+
+#     table = SingleTable(table_data)
+#     table.title = 'Model Index Scores'
+#     table.inner_row_border = True
+#     table.justify_columns[0] = 'right' 
+#     table.justify_columns[1] = 'center'
+#     table.justify_columns[2] = 'center'  
+
+#     print (table.table)
+
+# return_tabulated()
--- simulation.py
+++ simulation.py
@@ -1,7 +1,7 @@
 import random
-from states import states
 from contests import contests
 from terminaltables import AsciiTable, SingleTable
+from model_index import  scores
 import sys
 
 results = []
@@ -13,19 +13,22 @@ democrat_states = []
 republican_states = []
 
 def state_election():
-    for key, value in states.items():
+    for key, value in contests.items():
 
-        democratic_average = value[0]
-        republican_average = value[1]
+        democratic_average = value[1]
+        republican_average = value[2]
 
-        simulation = random.choices(['D', 'R'], [democratic_average, republican_average])
+        model_index_d = scores[key][0]
+        model_index_r = scores[key][1]
+
+        simulation = random.choices(['D', 'R'], [(int(democratic_average) * model_index_d), (int(republican_average) * model_index_r)])
         
         if 'D' in simulation:
-            democrat_electors.append(value[2])
+            democrat_electors.append(int(value[0]))
             democrat_states.append(key)
 
         if 'R' in simulation:
-            republican_electors.append(value[2])
+            republican_electors.append(int(value[0]))
             republican_states.append(key)
 
     if sum(democrat_electors) > sum(republican_electors):
@@ -43,8 +46,9 @@ def win_probability ():
     tie_probability = results.count('T') / 100
 
     table_data = [
-    ['DEMOCRAT', 'REPUBLICAN', 'TIE'],
-    [str(democrat_probability) + '%', str(republican_probability) + '%', str(tie_probability) + '%']
+    ['DEMOCRAT', 'REPUBLICAN', 'TIE', 'TOTAL'],
+    [str(democrat_probability) + '%', str(republican_probability) + '%', str(tie_probability) + '%',
+        str(democrat_probability + republican_probability + tie_probability) + '%']
     ]
     table = SingleTable(table_data)
     table.title = 'GE Chances [k=10000]'
@@ -55,18 +59,31 @@ def win_probability ():
     print (table.table)
 
 if __name__ == '__main__':
-    if sys.argv[1] == 'k':
+    if sys.argv[1] == 'simulate':
         for _ in range(10000):
             democrat_electors = []
             republican_electors = []
             state_election()
         win_probability()
 
-    if sys.argv[1] == 's':
+    if sys.argv[1] == 'single':
         state_election()
         se_table_data = [
         ['DEMOCRAT', 'REPUBLICAN', 'TOTAL'],
         [str(sum(democrat_electors)), str(sum(republican_electors)), str(sum(democrat_electors) + sum(republican_electors))]
+        ]
+        se_table = SingleTable(se_table_data)
+        se_table.title = 'GE Simulation'
+        se_table.justify_columns[0] = 'center' 
+        se_table.justify_columns[1] = 'center'
+        se_table.justify_columns[2] = 'center' 
+        print (se_table.table)
+
+    if sys.argv[1] == 'states':
+        state_election()
+        se_table_data = [
+        ['DEMOCRAT', 'REPUBLICAN', 'TOTAL'],
+        [democrat_states, republican_states, str(len(democrat_states) + len(republican_states))]
         ]
         se_table = SingleTable(se_table_data)
         se_table.title = 'GE Simulation'
