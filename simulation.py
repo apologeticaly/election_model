b3ec8b3337f8b6909b7f188e1ab5d3d5884fa12a
constantino <muheec@gmail.com>
2020-08-10 11:15:18 -0700

    simulation

--- simulation.py
+++ simulation.py
@@ -6,11 +6,15 @@ import sys
 import csv
 from termcolor import colored
 from itertools import zip_longest
+from progress.bar import Bar
 
 results = {}
 
 democrat_electors = []
 republican_electors = []
+
+democrat_states = []
+republican_states = []
 
 def state_election():
     for key, value in contests.items():
@@ -52,6 +56,8 @@ def state_election():
                 if democrat - republican > 10:
                     party = 1
 
+            democrat_states.append(name)
+
             results[id] = [name, ec, party, democrat_t, republican_t, third_t, margin_t, "D"]
 
         if democrat < republican:
@@ -71,33 +77,14 @@ def state_election():
                 if republican - democrat > 10:
                     party = 6
 
+            republican_states.append(name)
             results[id] = [name, ec, party, democrat_t, republican_t, third_t, margin_t, "R"]
 
-
-
-    
-# def win_probability ():
-#     # democrat_probability = results.count('D') / 100
-#     # republican_probability = results.count('R') / 100
-#     # tie_probability = results.count('T') / 100
-
-#     table_data = [
-#     [colored('DEMOCRAT', 'white', 'on_blue'), colored('REPUBLICAN', 'white', 'on_red'), 'TIE', 'TOTAL'],
-#     [str(democrat_probability) + '%', str(republican_probability) + '%', str(tie_probability) + '%',
-#         str(democrat_probability + republican_probability + tie_probability) + '%']
-#     ]
-#     table = SingleTable(table_data)
-#     table.title = 'GE Chances [k=10000]'
-#     table.justify_columns[0] = 'center' 
-#     table.justify_columns[1] = 'center'
-#     table.justify_columns[2] = 'center'  
-
-#     print (table.table)
 
 if __name__ == '__main__':
     if sys.argv[1] == 'rundown':
         temp_dump = []
-        for _ in range(1000):
+        for _ in range(100):
             state_election()
             if sum(democrat_electors) > sum(republican_electors):
                 temp_dump.append("D")
@@ -109,9 +96,93 @@ if __name__ == '__main__':
             republican_electors = []
         print(temp_dump.count("D"), temp_dump.count("R"))
 
+    if sys.argv[1] == 'single':
+        state_election()
+        se_table_data = [
+        [colored('DEMOCRAT', 'white', 'on_blue'), colored('REPUBLICAN', 'white', 'on_red'), 'TOTAL'],
+        [str(sum(democrat_electors)), str(sum(republican_electors)), str(sum(democrat_electors) + sum(republican_electors))]
+        ]
+        se_table = SingleTable(se_table_data)
+        se_table.title = 'GE Simulation'
+        se_table.justify_columns[0] = 'center' 
+        se_table.justify_columns[1] = 'center'
+        se_table.justify_columns[2] = 'center' 
+        print (se_table.table)
+
+    if sys.argv[1] == 'states':
+        out = 0
+        for _ in range(100):
+            state_election()
+            with open(f'temp/2020_weighted/{out}_2020_weighted.csv', 'w') as csvfile:
+                filewriter = csv.writer(csvfile,
+                                        quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                filewriter.writerow(["name", "ec", "margin", "winner"])
+                
+                for key, value in results.items():
+                    filewriter.writerow([value[0], value[1], value[6], value[7]])
+                    
+                filewriter.writerow([sum(democrat_electors), sum(republican_electors)])
+            print('File outputed at temp/2020/' + str(out) + '_2020.csv')
+            out = out + 1
+            democrat_electors = []
+            republican_electors = []
+
+    if sys.argv[1] == 'prob':
+        bar = Bar('Processing', max=100)
+        temp_dump = []
+
+        dem = 0
+        rep = 0
+
+        for _ in range(100):
+            state_election()
+            dem = dem + sum(democrat_electors)
+            rep = rep + sum(republican_electors)
+
+            if sum(democrat_electors) > sum(republican_electors):
+                temp_dump.append("D")
+            
+            if sum(democrat_electors) < sum(republican_electors):
+                temp_dump.append("R")
+
+            democrat_electors = []
+            republican_electors = []
+            bar.next()
+
+        democrat_probability = temp_dump.count('D')
+        republican_probability = temp_dump.count('R')
+        tie_probability = temp_dump.count('T')
+
+        table_data = [
+        [colored('DEMOCRAT', 'white', 'on_blue'), colored('REPUBLICAN', 'white', 'on_red'), 'TIE', 'TOTAL'],
+        [str(democrat_probability) + '%', str(republican_probability) + '%', str(tie_probability) + '%',
+            str(democrat_probability + republican_probability + tie_probability) + '%']
+        ]
+
+        table_data_ec = [
+        [colored('DEMOCRAT', 'white', 'on_blue'), colored('REPUBLICAN', 'white', 'on_red')],
+        [str(dem/100), str(rep/100)]
+        ]
+
+        table = SingleTable(table_data)
+        table.title = 'GE Win Chances [k=100]'
+        table.justify_columns[0] = 'center' 
+        table.justify_columns[1] = 'center'
+        table.justify_columns[2] = 'center'  
+
+        table_ec = SingleTable(table_data_ec)
+        table_ec.title = 'Average EC Votes [k=100]'
+        table_ec.justify_columns[0] = 'center' 
+        table_ec.justify_columns[1] = 'center'
+        table_ec.justify_columns[2] = 'center'  
+
+        bar.finish()
+        print (table.table)
+        print (table_ec.table)
+
     elif sys.argv[1] == 'results':
         state_election()
-        with open('results.csv', 'w') as csvfile:
+        with open('results_.csv', 'w') as csvfile:
             filewriter = csv.writer(csvfile,
                                     quotechar='|', quoting=csv.QUOTE_MINIMAL)
             filewriter.writerow(["id", "name", "ec", "party", "democrat", "republican", "third", "margin", "winner"])
@@ -121,48 +192,3 @@ if __name__ == '__main__':
                 
             filewriter.writerow([sum(democrat_electors), sum(republican_electors)])
         print('File outputed as results.csv')
-
-# if __name__ == '__main__':
-#     try:
-#         if sys.argv[1] == 'single':
-#             state_election()
-#             se_table_data = [
-#             [colored('DEMOCRAT', 'white', 'on_blue'), colored('REPUBLICAN', 'white', 'on_red'), 'TOTAL'],
-#             [str(sum(democrat_electors)), str(sum(republican_electors)), str(sum(democrat_electors) + sum(republican_electors))]
-#             ]
-#             se_table = SingleTable(se_table_data)
-#             se_table.title = 'GE Simulation'
-#             se_table.justify_columns[0] = 'center' 
-#             se_table.justify_columns[1] = 'center'
-#             se_table.justify_columns[2] = 'center' 
-#             print (se_table.table)
-
-#         if sys.argv[1] == 'states':
-#             state_election()
-#             se_table_data = [
-#             [colored('DEMOCRAT', 'white', 'on_blue'), colored('REPUBLICAN', 'white', 'on_red'), 'TOTAL'],
-#             ['\n'.join([str(i) for i in democrat_states]), '\n'.join([str(i) for i in republican_states]), str(len(democrat_states) + len(republican_states))],
-#             [sum(democrat_electors), sum(republican_electors), (sum(democrat_electors)+ sum(republican_electors))]
-#             ]
-#             se_table = SingleTable(se_table_data)
-#             se_table.title = 'GE Simulation'
-#             se_table.inner_row_border = True
-#             se_table.justify_columns[0] = 'center' 
-#             se_table.justify_columns[1] = 'center'
-#             se_table.justify_columns[2] = 'center' 
-#             print(se_table.table)
-
-#         elif sys.argv[1] == 'csv':
-#             state_election()
-#             with open('results.csv', 'w') as csvfile:
-#                 filewriter = csv.writer(csvfile,
-#                                         quotechar='|', quoting=csv.QUOTE_MINIMAL)
-#                 filewriter.writerow(["id", "name", "ec", "party", "democrat", "republican", "third", "margin", "winner"])
-#                 for i in results:
-#                     filewriter.writerow([i,"D", ])
-
-#                 for i in results:
-#                     filewriter.writerow([i,"R"])
-                
-#                 filewriter.writerow([sum(democrat_electors), sum(republican_electors)])
-#             print('File outputed as output.csv') 
