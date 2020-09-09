cc296f44c4681f6689e83cd56a04066b478975db
constantino <muheec@gmail.com>
2020-09-09 11:47:50 -0700

    priors

--- contests.py
+++ contests.py
@@ -1,9 +1,5 @@
 import csv
-<<<<<<< HEAD
-with open('states_weighted.csv', mode='r') as infile:
-=======
-with open('states_august_weighted.csv', mode='r') as infile:
->>>>>>> 7d5df8b66ea21f079d224bcc83d0e424da47c1b7
+with open('input/states_august_weighted.csv', mode='r') as infile:
     reader = csv.reader(infile)
     contests = {rows[0]:rows[1:11] for rows in reader}
     del contests['State']
